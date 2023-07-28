66c17ab356bcd476b1c4893885e0bea169853a11
Henrique Altmayer <henrique_altm@hotmail.com>
2023-07-28 15:34:44 -0300

    Initial setup

---
+++ app/src/main/java/com/example/composeplayground/TestApplication.kt
@@ -0,0 +1,8 @@
+package com.example.composeplayground
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import dagger.hilt.android.testing.CustomTestApplication
+
+@HiltAndroidApp
+class TestApplication : Application()
